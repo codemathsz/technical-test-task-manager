6e0045ecae4cd9baeeabbad17f9cac0e39ab0825
codemathsz <mo284891@gmail.com>
2025-01-10 11:09:58 -0300

    feat: change api url

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: 'http://localhost:8080'
+  baseURL: 'https://technical-test-task-manager-api-production.up.railway.app/'
 });
